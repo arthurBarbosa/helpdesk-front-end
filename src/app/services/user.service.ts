4197a5b1e4c7e359d90ff867728d8a1077b2cbfa
Arthur Barbosa <arthurbruno03@gmail.com>
2018-09-15 11:45:06 -0300

    criação da classe de Usuario e Usuario.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../model/user.model';
+import {HELP_DESK_API} from './helpdesk.api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(user : User){
+    return this.http.post(`${HELP_DESK_API}/api/auth`, user);
+  }
 }
